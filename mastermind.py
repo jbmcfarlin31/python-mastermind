971468c61275a0abdc62b07b46bd274ac09020f4
Jonathon McFarlin <38010699+jbmcfarlin31@users.noreply.github.com>
2019-05-06 17:03:45 -0600

    Create mastermind.py

---
+++ mastermind.py
@@ -0,0 +1,132 @@
+import random
+import re
+
+
+COLORS = {
+	1: 'r',
+	2: 'b',
+	3: 'g',
+	4: 'w',
+	5: 'y'
+}
+
+MASTER_SOLUTION = []
+
+PATTERN = "^[r,b,g,w,y]+$"
+
+
+def check_answer(answer, nbr_colors):
+
+	def check_result(ans,ms):
+		if ans <= ms:
+			result = ans
+		else:
+			result = ms
+
+		return result
+
+	global MASTER_SOLUTION
+
+	check1, check2, check3, check4, check5 = 0,0,0,0,0
+
+	r_cnt_ms, b_cnt_ms, g_cnt_ms, w_cnt_ms, y_cnt_ms = 0,0,0,0,0
+	r_cnt_ans, b_cnt_ans, g_cnt_ans, w_cnt_ans, y_cnt_ans = 0,0,0,0,0
+	r_result, b_result, g_result, w_result, y_result = 0,0,0,0,0
+
+
+	if answer[0] == MASTER_SOLUTION[0]:
+		check1 = 1
+
+	if answer[1] == MASTER_SOLUTION[1]:
+		check2 = 1
+
+	if answer[2] == MASTER_SOLUTION[2]:
+		check3 = 1
+
+	if answer[3] == MASTER_SOLUTION[3]:
+		check4 = 1
+
+	if answer[4] == MASTER_SOLUTION[4]:
+		check5 = 1
+
+	correct_answer_in_correct_space = check1 + check2 + check3 + check4 + check5
+
+	# Default is always 2, so we will always have the values
+	r_cnt_ms = MASTER_SOLUTION.count("r")
+	r_cnt_ans = answer.count("r")
+
+	r_result = check_result(r_cnt_ans, r_cnt_ms)
+
+	b_cnt_ms = MASTER_SOLUTION.count("b")
+	b_cnt_ans = answer.count("b")
+
+	b_result = check_result(b_cnt_ans, b_cnt_ms)
+
+	# If they choose more than 2 colors we handle it here
+	if nbr_colors > 2:
+		g_cnt_ms = MASTER_SOLUTION.count("g")
+		g_cnt_ans = answer.count("g")
+		g_result = check_result(g_cnt_ans, g_cnt_ms)
+
+	if nbr_colors > 3:
+		w_cnt_ms = MASTER_SOLUTION.count("w")
+		w_cnt_ans = answer.count("w")
+		w_result = check_result(w_cnt_ans, w_cnt_ms)
+
+	if nbr_colors > 4:
+		y_cnt_ms = MASTER_SOLUTION.count("y")
+		y_cnt_ans = answer.count("y")
+		y_result = check_result(y_cnt_ans, y_cnt_ms)
+
+	correct_answer_in_wrong_space = r_result + b_result + g_result + w_result + y_result
+	correct_answer_in_wrong_space = correct_answer_in_wrong_space - correct_answer_in_correct_space
+
+
+	print('\n')
+	print("You entered {}".format(answer))
+	print("The correct number in the correct spot was {}".format(correct_answer_in_correct_space))
+	print("The correct number in the wrong spot was {}".format(correct_answer_in_wrong_space))
+	print('\n')
+
+	if correct_answer_in_correct_space == 5:
+		end_game = True
+	else:
+		end_game = False
+
+
+	return end_game
+
+# ---------------------------------------------------
+
+nbr_colors = input("How many colors do you want to play with? (2-5): ")
+nbr_colors = int(nbr_colors)
+
+color_list = []
+for x in range(nbr_colors):
+	color_list.append(COLORS[x+1])
+
+for x in range(5):
+	rand_nbr = random.randint(1,nbr_colors)
+	MASTER_SOLUTION.append(COLORS[rand_nbr])
+
+print("[DEBUG] The MASTER SOLUTION is:",MASTER_SOLUTION,'\n')
+
+print("You are playing with {} colors. Your choices can be 5 of the following letters: {})\n".format(nbr_colors, color_list))
+
+end_game = False
+while end_game == False:
+
+	answer = input("What 5 colors would you like to try?: {} ".format(color_list))
+	answer = str(answer)
+
+	if re.search(PATTERN,answer):
+		if len(answer) == 5:
+			# Check user answer with MASTER_SOLUTION
+			end_game = check_answer(answer, nbr_colors)
+		else:
+			print("You must choose 5 letters.")
+	else:
+		print("You are playing with {} colors. Your choices can be 5 of the following letters: {})".format(nbr_colors, color_list))
+
+
+print("Congratulations, you won!!!")
